63118542e5eb02b93d2304b6bf00493a54bf984c
Diego Pardo Montero <diegopardomontero@gmail.com>
2024-06-05 17:02:14 -0500

    Decouple object methods and iterations

--- Advanced/LoopOverObjects.js
+++ Advanced/LoopOverObjects.js
@@ -1,20 +1,10 @@
-//Keys:
-const car2 = {
-    speed: 200,
-    color: "red"
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
 }
-console.log(Object.keys(car2)); // ['speed','color']
 
-//Values:
-const car3 = {
-    speed: 300,
-    color: "yellow"
+for( const key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
 }
-console.log(Object.values(car3)); // [300, 'yellow']
-
-//Entries:
-const car4 = {
-    speed: 400,
-    color: 'magenta'
-} 
-console.log(Object.entries(car4)); //[ ['speed', 400], ['color', 'magenta'] ]
---
+++ Advanced/ObjectMethods.js
@@ -0,0 +1,20 @@
+//Keys:
+const car2 = {
+    speed: 200,
+    color: "red"
+}
+console.log(Object.keys(car2)); // ['speed','color']
+
+//Values:
+const car3 = {
+    speed: 300,
+    color: "yellow"
+}
+console.log(Object.values(car3)); // [300, 'yellow']
+
+//Entries:
+const car4 = {
+    speed: 400,
+    color: 'magenta'
+} 
+console.log(Object.entries(car4)); //[ ['speed', 400], ['color', 'magenta'] ]
