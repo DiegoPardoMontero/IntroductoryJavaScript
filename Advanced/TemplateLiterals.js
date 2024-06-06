6c5551a1fe24dbea8ea6ee04c5f6ce1a35d6d6ca
Diego Pardo Montero <diegopardomontero@gmail.com>
2024-06-05 19:20:35 -0500

    Add example using template literals

---
+++ Advanced/TemplateLiterals.js
@@ -0,0 +1,10 @@
+var subject = "Mundo!"
+var multiLine = `
+                This
+                is 
+                a 
+                great
+                example!`
+
+console.log(`Hola ${subject}`)
+console.log(multiLine)
